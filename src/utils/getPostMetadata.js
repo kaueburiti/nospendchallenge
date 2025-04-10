dfcfffa20ed1198f0fccf9468d56b0c904dbd333
Débora Teodoríco <debora.steodorico@gmail.com>
2025-04-10 10:47:16 -0300

    feat: create getPostContent

--- src/utils/getPostMetadata.js
+++ src/utils/getPostMetadata.js
@@ -17,3 +17,40 @@ export default function getPostMetadata(basePath) {
 
 	return posts;
 }
+
+export function getPostContent(slug) {
+	const folder = 'blog';
+	const file = folder + `/${slug}.md`;
+	const content = fs.readFileSync(file, 'utf8');
+	const matterResult = matter(content);
+
+	return matterResult;
+}
+
+export const generateStaticParams = async () => {
+	const posts = getPostMetadata('blog');
+
+	return posts.map((post) => ({
+		slug: post.slug,
+	}));
+};
+
+export async function generateMetadata({ params, searchParams }) {
+	const id = params?.slug ? ' . ' + params?.slug : '';
+	return {
+		title: `Blog ${id.replaceAll('_', ' ')}`,
+		description: 'Blog page',
+		openGraph: {
+			title: 'Blog' + id,
+			description: 'Blog page',
+			url: 'https://example.com/blog' + id,
+			images: [
+				{
+					url: 'https://example.com/image.jpg',
+					width: 800,
+					height: 600,
+				},
+			],
+		},
+	};
+}
