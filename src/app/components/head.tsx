90b84792b3290cd03e81859603c67e92ab5ad715
Débora Teodoríco <debora.steodorico@gmail.com>
2025-04-23 16:15:03 -0300

    feate: create heading component and add canonical tag

---
+++ src/app/components/head.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function HeadComponent() {
+	return (
+		<Head>
+			<title>No Spend Challenge - Stop Impulsive Buying</title>
+			<meta
+				name='description'
+				content='Join the No Spend Challenge and learn how to save money effectively. Take control of your finances today!'
+			/>
+			<link rel='canonical' href='https://nospendchallenge.app/' />
+			<meta
+				name='google-site-verification'
+				content='8TRi_yA8pcOP7kEIGljcMNl5-ef84YxEgH3-9oU4OJk'
+			/>
+		</Head>
+	);
+}
