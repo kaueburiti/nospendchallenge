0674d919ff49fac62e32ead6b87fa54583133403
Débora Teodoríco <debora.steodorico@gmail.com>
2025-04-23 16:17:36 -0300

    feat: delete unnecessary code

--- src/app/components/head.tsx
+++ src/app/components/head.tsx
@@ -4,10 +4,10 @@ import Head from 'next/head';
 export default function HeadComponent() {
 	return (
 		<Head>
-			<title>No Spend Challenge - Stop Impulsive Buying</title>
+			<title>#NoSpendChallenge - Stop Impulsive Buying</title>
 			<meta
 				name='description'
-				content='Join the No Spend Challenge and learn how to save money effectively. Take control of your finances today!'
+				content='Join the #NoSpendChallenge and learn how to save money effectively. Take control of your finances today!'
 			/>
 			<link rel='canonical' href='https://nospendchallenge.app/' />
 			<meta
