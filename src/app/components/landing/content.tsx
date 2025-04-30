851f140f6475a2541aa645ecb490af65cdfe6498
Kauê Buriti <kauepbd@gmail.com>
2025-04-30 15:08:26 -0300

    📃 Improve landing page content

--- src/app/components/landing/content.tsx
+++ src/app/components/landing/content.tsx
@@ -20,16 +20,17 @@ export default async function LandingPageContent() {
               className="grid grid-cols-1 mt-10 text-center wow animate__animated animate__fadeInUp"
               data-wow-delay=".1s"
             >
-              <h3 className="font-bold lg:leading-normal leading-normal text-4xl lg:text-[56px] mb-5">
-                Stop impulsive buying
-              </h3>
+              <h1 className="font-bold lg:leading-normal leading-normal text-4xl lg:text-[56px] mb-4">
+                Stop Impulsive Buying with
+                <br /> No Spend Challenge!
+              </h1>
 
-              <p className="text-slate-500 text-lg max-w-xl mx-auto mb-4">
-                Create challenges that will help you on your journey to
-                financial control, avoiding consumerism and impulse buying
-              </p>
+              <h2 className="text-slate-500 text-lg max-w-xl mx-auto mb-4">
+                Save money and track your spendings by creating challenges that
+                guides you to financial control one step per day!
+              </h2>
 
-              <div className="flex mt-6 flex-col gap-2 justify-center">
+              <div className="flex mt-6 flex-col gap-1 justify-center">
                 <div className=" flex-1">
                   <a
                     href="https://apps.apple.com/us/app/nospendchallenge/id6744117715"
@@ -45,12 +46,13 @@ export default async function LandingPageContent() {
                     >
                       <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
                     </svg>
-                    Download iOS App
+                    Download App for{" "}
+                    <span className="underline ml-1">Free</span>
                   </a>
                 </div>
 
                 <div className="flex-1">
-                  <p className="text-gray-500 rounded-full px-6 py-3 inline-flex items-center text-sm">
+                  <p className="text-gray-500 rounded-full px-6 py-3 inline-flex items-center text-xs">
                     Coming soon to Android platform
                   </p>
                 </div>
@@ -106,7 +108,7 @@ export default async function LandingPageContent() {
                 We're not a new finance tracking app.
               </h6>
               <h4 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-bold">
-                Take back the control of your wallet
+                Take back the control of your budget
               </h4>
 
               <p className="text-slate-400 max-w-xl mx-auto">
@@ -200,23 +202,32 @@ export default async function LandingPageContent() {
                 to financial freedom.
               </p>
 
-              <div className="flex justify-center">
-                <a
-                  href="https://apps.apple.com/us/app/nospendchallenge/id6744117715"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn bg-red-500 hover:bg-red-600 text-white rounded-full px-6 py-3 inline-flex items-center"
-                >
-                  <svg
-                    className="w-5 h-5 me-2"
-                    viewBox="0 0 384 512"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
+              <div className="flex justify-center flex-col gap-1">
+                <div className="flex-1">
+                  <a
+                    href="https://apps.apple.com/us/app/nospendchallenge/id6744117715"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn bg-red-500 hover:bg-red-600 text-white rounded-full px-6 py-3 inline-flex items-center"
                   >
-                    <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
-                  </svg>
-                  Download iOS App
-                </a>
+                    <svg
+                      className="w-5 h-5 me-2"
+                      viewBox="0 0 384 512"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
+                    </svg>
+                    Download App for{" "}
+                    <span className=" ml-1 underline">Free</span>
+                  </a>
+                </div>
+
+                <div className="flex-1">
+                  <p className="text-gray-500 rounded-full px-6 py-3 inline-flex items-center text-xs">
+                    Coming soon to Android platform
+                  </p>
+                </div>
               </div>
 
               {/* <div className="mt-8 flex items-center">
